8f08a3bfe91f1e028a163717855a8fc652f07a85
Peter Dalbhanjan <dalbhanj@amazon.com>
2022-09-09 16:08:23 -0400

    adding references

--- pkg/resource/job_run/sdk.go
+++ pkg/resource/job_run/sdk.go
@@ -467,9 +467,6 @@ func (rm *resourceManager) getImmutableFieldChanges(
 	delta *ackcompare.Delta,
 ) []string {
 	var fields []string
-	if delta.DifferentAt("Spec.VirtualClusterID") {
-		fields = append(fields, "VirtualClusterID")
-	}
 	if delta.DifferentAt("Spec.ExecutionRoleARN") {
 		fields = append(fields, "ExecutionRoleARN")
 	}
@@ -481,6 +478,9 @@ func (rm *resourceManager) getImmutableFieldChanges(
 	}
 	if delta.DifferentAt("Spec.ReleaseLabel") {
 		fields = append(fields, "ReleaseLabel")
+	}
+	if delta.DifferentAt("Spec.VirtualClusterID") {
+		fields = append(fields, "VirtualClusterID")
 	}
 
 	return fields
