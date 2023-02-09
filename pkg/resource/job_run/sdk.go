ab9a29959421483d3b6b1bdd7c06f1412aeff51d
ci-robot <ci-robot@k8s.io>
2023-02-09 23:12:32 +0000

    Update to ACK runtime `v0.24.0`, code-generator `v0.24.2`

--- pkg/resource/job_run/sdk.go
+++ pkg/resource/job_run/sdk.go
@@ -490,15 +490,6 @@ func (rm *resourceManager) getImmutableFieldChanges(
 	delta *ackcompare.Delta,
 ) []string {
 	var fields []string
-	if delta.DifferentAt("Spec.Name") {
-		fields = append(fields, "Name")
-	}
-	if delta.DifferentAt("Spec.ReleaseLabel") {
-		fields = append(fields, "ReleaseLabel")
-	}
-	if delta.DifferentAt("Spec.VirtualClusterId") {
-		fields = append(fields, "VirtualClusterId")
-	}
 	if delta.DifferentAt("Spec.ConfigurationOverrides") {
 		fields = append(fields, "ConfigurationOverrides")
 	}
@@ -507,6 +498,15 @@ func (rm *resourceManager) getImmutableFieldChanges(
 	}
 	if delta.DifferentAt("Spec.JobDriver") {
 		fields = append(fields, "JobDriver")
+	}
+	if delta.DifferentAt("Spec.Name") {
+		fields = append(fields, "Name")
+	}
+	if delta.DifferentAt("Spec.ReleaseLabel") {
+		fields = append(fields, "ReleaseLabel")
+	}
+	if delta.DifferentAt("Spec.VirtualClusterId") {
+		fields = append(fields, "VirtualClusterId")
 	}
 
 	return fields
