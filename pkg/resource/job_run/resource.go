5c9db0ed8ff42f25b4b0fe77a18a5a20c22698d5
ci-robot <ci-robot@k8s.io>
2024-12-09 17:29:16 +0000

    Update to ACK runtime `v0.40.0`, code-generator `v0.40.0`

--- pkg/resource/job_run/resource.go
+++ pkg/resource/job_run/resource.go
@@ -98,6 +98,22 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 	return nil
 }
 
+// PopulateResourceFromAnnotation populates the fields passed from adoption annotation
+func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) error {
+	tmp, ok := fields["id"]
+	if !ok {
+		return ackerrors.MissingNameIdentifier
+	}
+	r.ko.Status.ID = &tmp
+
+	f1, f1ok := fields["virtualClusterID"]
+	if f1ok {
+		r.ko.Spec.VirtualClusterID = &f1
+	}
+
+	return nil
+}
+
 // DeepCopy will return a copy of the resource
 func (r *resource) DeepCopy() acktypes.AWSResource {
 	koCopy := r.ko.DeepCopy()
